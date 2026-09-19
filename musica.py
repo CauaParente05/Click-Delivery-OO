ad43f0d63ee9281b0510aeddf3d99792a739993a
Cauã Parente <cauahenriqueccpar@gmail.com>
2026-09-19 16:39:55 -0300

    Primeiro Commit

---
+++ musica.py
@@ -0,0 +1,20 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = float
+    
+m1 = Musica()
+
+m1.nome = 'Beat it'
+m1.artista = 'Michael Jackson'
+m1.duracao = 4.20
+    
+m2 = Musica()
+m2.nome = 'Bohemian Rhapsody'
+m2.artista = 'Queen'
+m2.duracao = 355
+
+m3 = Musica()
+m3.nome = 'Shape of You'
+m3.artista = 'Ed Sheeran'
+m3.duracao = 234
